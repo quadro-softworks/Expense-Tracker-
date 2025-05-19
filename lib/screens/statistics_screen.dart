6e055760454e39e695e46a99eaeaf5f7196dc3b9
BaslaelWorkineh <baslaworku@gmail.com>
2025-05-19 11:00:00 +0300

    refactor: Update placeholder content in StatisticsScreen standalone file

--- lib/screens/statistics_screen.dart
+++ lib/screens/statistics_screen.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 
-    class StatisticsScreen extends StatelessWidget {
-      const StatisticsScreen({super.key});
+class StatisticsScreen extends StatelessWidget {
+  const StatisticsScreen({super.key});
 
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Statistics'),
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          ),
-          body: const Center(
-            child: Text('Statistics will be shown here.'),
-          ),
-        );
-      }
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Statistics'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.pie_chart_outline, size: 64, color: Colors.grey),
+            SizedBox(height: 16),
+            Text(
+              'Detailed expense statistics are coming soon!',
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
